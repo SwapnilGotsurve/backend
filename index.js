34652d05dbc4d55cba022c711703f295d8936885
swapnilgotsurve <swapnilgotsurve5042@gmail.com>
2025-11-14 22:31:28 +0530

    github data added

--- index.js
+++ index.js
@@ -2,7 +2,54 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 3000
+
+const githubData ={
+  "username": "swapnil",
+  "fullName": "Swapnil Patil",
+  "bio": "Full-stack developer | Open-source enthusiast | Learning daily",
+  "location": "Pune, India",
+  "profileImage": "https://example.com/profile.jpg",
+  "followers": 120,
+  "following": 85,
+  "publicRepos": 12,
+  "githubURL": "https://github.com/swapnil",
+  "repositories": [
+    {
+      "repoName": "portfolio-website",
+      "description": "Personal portfolio built with HTML, CSS, JS",
+      "language": "JavaScript",
+      "stars": 15,
+      "forks": 3,
+      "updatedAt": "2025-01-12"
+    },
+    {
+      "repoName": "django-blog-app",
+      "description": "A blog application built using Django",
+      "language": "Python",
+      "stars": 28,
+      "forks": 6,
+      "updatedAt": "2025-02-05"
+    },
+    {
+      "repoName": "todo-react-app",
+      "description": "A simple Todo app using React",
+      "language": "JavaScript",
+      "stars": 12,
+      "forks": 2,
+      "updatedAt": "2025-03-01"
+    }
+  ],
+  "skills": [
+    "Python",
+    "JavaScript",
+    "Django",
+    "React",
+    "Git",
+    "HTML",
+    "CSS"
+  ]
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -12,6 +59,9 @@ app.get("/home", (req, res) => {
 })
 app.get("/youtube", (req, res) => {
     res.send("<h1> Welcome to Youtube </h1>")
+})
+app.get("/github", (req, res) => {
+  res.json(githubData)
 })
 app.listen( process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
